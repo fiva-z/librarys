53e3d7b1415bce0fbb97ab2b61a327a1330e0ae0
fx fx fx <105108559+fiva-z@users.noreply.github.com>
2026-03-15 02:50:39 +0100

    Change AccentColor to blue in Library.lua

--- SBxFreemium/Library.lua
+++ SBxFreemium/Library.lua
@@ -32,7 +32,7 @@ local Library = {
     FontColor = Color3.fromRGB(255, 255, 255);
     MainColor = Color3.fromRGB(28, 28, 28);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
-    AccentColor = Color3.fromRGB(5, 95, 210);
+    AccentColor = Color3.fromRGB(0, 0, 255);
     OutlineColor = Color3.fromRGB(50, 50, 50);
     RiskColor = Color3.fromRGB(255, 50, 50),
 
@@ -2992,11 +2992,11 @@ function Library:CreateWindow(...)
 
     local titleParts = {
         {text = " FX ", color = Color3.fromRGB(255, 255, 255)},
-        {text = "Scripts", color = Color3.fromRGB(5, 95, 210)},
+        {text = "Scripts", color = Color3.fromRGB(0, 0, 255)},
         {text = "  |  ", color = Color3.fromRGB(255, 255, 255)},
 		{text = "discord.gg/Neqa57vzHr", color = Color3.fromRGB(255, 255, 255)},
 		{text = "  |  ", color = Color3.fromRGB(255, 255, 255)},
-		{text = "Freemium", color = Color3.fromRGB(5, 95, 210)},
+		{text = "Freemium", color = Color3.fromRGB(0, 0, 255)},
         {text = "  |  ", color = Color3.fromRGB(255, 255, 255)},
 		{text = "SouthBronx", color = Color3.fromRGB(255, 255, 255)},
     }
