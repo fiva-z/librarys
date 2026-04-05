098034d136225beef0d0a956e6b315a85c2dbe64
fiva <105108559+fiva-z@users.noreply.github.com>
2026-04-05 19:01:03 +0200

    Update ThemeManager.lua

--- SBxFreemium/ThemeManager.lua
+++ SBxFreemium/ThemeManager.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"131313","AccentColor":"ff00ff","BackgroundColor":"0e0e0e","OutlineColor":"141419"}') },,
+		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"131313","AccentColor":"0000ff","BackgroundColor":"0e0e0e","OutlineColor":"141419"}') },
 		['BBot'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Fatality']		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
 		['Jester'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
