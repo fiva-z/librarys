53e1ade5370dd694c727cbafb5b5ffa4e8f8e826
fiva <105108559+fiva-z@users.noreply.github.com>
2026-04-05 18:34:12 +0200

    Update Library.lua

--- SBxFreemium/Library.lua
+++ SBxFreemium/Library.lua
@@ -2994,14 +2994,14 @@ function Library:CreateWindow(...)
     local daySuffix = getDaySuffix(day)
 
     local titleParts = {
-        {text = "  Fiva", color = Color3.fromRGB(255, 255, 255)},
-        {text = "   |   ", color = Color3.fromRGB(255, 255, 255)},
+        {text = "   Fiva", color = Color3.fromRGB(255, 255, 255)},
+        {text = "       |       ", color = Color3.fromRGB(255, 255, 255)},
 		{text = ".gg/Neqa57vzHr", color = Color3.fromRGB(0, 0, 255)},
-		{text = "   |   ", color = Color3.fromRGB(255, 255, 255)},
+		{text = "       |       ", color = Color3.fromRGB(255, 255, 255)},
 		{text = "Freemium Version", color = Color3.fromRGB(255, 255, 255)},
-        {text = "   |   ", color = Color3.fromRGB(255, 255, 255)},
+        {text = "       |       ", color = Color3.fromRGB(255, 255, 255)},
 		{text = "South Bronx", color = Color3.fromRGB(0, 0, 255)},
-		{text = "   |  ", color = Color3.fromRGB(255, 255, 255)},
+		{text = "       |      ", color = Color3.fromRGB(255, 255, 255)},
         {text = " " .. executorname, color = Color3.fromRGB(0, 0, 255)}
     }
 
